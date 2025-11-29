02d6868591587932f8ccc0bbef8ff0874c196130
Nikita <129417335+REFLXS@users.noreply.github.com>
2025-11-29 21:44:26 +0400

    add Stamina Bar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,11 +15,23 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _direction;
     private Vector3 _velocity;
 
+    public Image StaminaBar;
+    public float Stamina, MaxStamina;
+    public float EventCost;
+    public float RunCost;
+    public float StaminaRegenRate = 10f;
+
+    private bool _isRunning;
+    private bool _canRun = true;
+
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
         _playerCamera = GetComponentInChildren<Camera>();
         Cursor.lockState = CursorLockMode.Locked;
+ 
+        Stamina = MaxStamina;
+        UpdateStaminaBar();
     }
 
     void Update()
@@ -38,14 +51,74 @@ public class PlayerMovement : MonoBehaviour
         _rotation.y += mouseDelta.x;
         _rotation.x = Mathf.Clamp(_rotation.x - mouseDelta.y, -90, 90);
         _playerCamera.transform.localEulerAngles = _rotation;
+
+        HandleRunning();
+        RegenerateStamina();
+
+        if (Input.GetKeyDown("f"))
+        {
+            Debug.Log("Действие выполнено");
+            Stamina -= EventCost;
+            if(Stamina < 0) Stamina = 0;
+            UpdateStaminaBar();
+        }
     }
 
     void FixedUpdate()
     {
         if (!enabled) return;
+
+        float currentSpeed = _isRunning && _canRun ? _runSpeed : _walkSpeed;
+        _direction *= currentSpeed;
         
-        _direction *= Input.GetKey(KeyCode.LeftShift) ? _runSpeed : _walkSpeed;
         Vector3 move = Quaternion.Euler(0, _playerCamera.transform.eulerAngles.y, 0) * new Vector3(_direction.x, 0, _direction.y);
         _velocity = new Vector3(move.x, _velocity.y, move.z);
+    }
+
+    private void HandleRunning()
+    {
+        bool wantsToRun = Input.GetKey(KeyCode.LeftShift) && _direction.magnitude > 0.1f;
+        
+        if (wantsToRun && _canRun && Stamina > 0)
+        {
+            _isRunning = true;
+            Stamina -= RunCost * Time.deltaTime;
+
+            if (Stamina <= 0)
+            {
+                Stamina = 0;
+                _canRun = false;
+                _isRunning = false;
+            }
+            
+            UpdateStaminaBar();
+        }
+        else
+        {
+            _isRunning = false;
+
+            if (!_canRun && Stamina >= MaxStamina * 0.3f)
+            {
+                _canRun = true;
+            }
+        }
+    }
+
+    private void RegenerateStamina()
+    {
+        if (!_isRunning && Stamina < MaxStamina)
+        {
+            Stamina += StaminaRegenRate * Time.deltaTime;
+            if (Stamina > MaxStamina)
+                Stamina = MaxStamina;
+            
+            UpdateStaminaBar();
+        }
+    }
+
+    private void UpdateStaminaBar()
+    {
+        if (StaminaBar != null)
+            StaminaBar.fillAmount = Stamina / MaxStamina;
     }
 }
